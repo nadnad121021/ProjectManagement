5bc20dc70d3e00a6e33d2f877f42d14fa5884fd5
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-18 09:21:37 +0800

    files modified

--- routes/managerRoutes/getRoutes.js
+++ routes/managerRoutes/getRoutes.js
@@ -1,5 +1,23 @@
 module.exports = function(app){
-    app.get('/ManagerDashboard',function(req,res){
-        res.render('../views/managertakenProject.ejs');
+    app.get('/manager_takenProjects',function(req,res){
+        res.render('../views/manager_takenProjects.ejs');
+    })
+    app.get('/manager_addTasks',function(req,res){
+        res.render('../views/manager_addTasks.ejs');
+    })
+    app.get('/manager_clients',function(req,res){
+        res.render('../views/manager_clients.ejs');
+    })
+    app.get('/manager_viewTasks',function(req,res){
+        res.render('../views/manager_viewTasks.ejs');
+    })
+    app.get('/manager_openProjects',function(req,res){
+        res.render('../views/manager_openProjects.ejs');
+    })
+    app.get('/manager_addProjects',function(req,res){
+        res.render('../views/manager_addProjects.ejs');
+    })
+    app.get('/manager_assignedProjects',function(req,res){
+        res.render('../views/manager_assignedProjects');
     })
 }
