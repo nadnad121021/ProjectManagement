a3e6d6c7dcb46a972f2fde24bf0268f1dd864a66
unknown <2ndyrGroupA@PN13.pn.local>
2019-05-21 20:35:51 +0800

    files modified

--- routes/managerRoutes/getRoutes.js
+++ routes/managerRoutes/getRoutes.js
@@ -47,10 +47,11 @@ module.exports = function(app){
             var Clients = doc;
             db.collection('Projects').find({"Type":"Open"}).toArray(function(err,open){
                 var Open = open;
-                db.collection('Projects').find({"Type":"Taken"}).toArray(function(err,taken){
-                     //console.log(taken)
-                     res.render('../views/manager_assignedClients.ejs',{OpenProjects:Open,TakenProjects:taken,Users:Clients});
-                })
+                res.render('../views/manager_assignedClients.ejs',{OpenProjects:Open,Users:Clients});
+                // db.collection('Projects').find({"Type":"Taken"}).toArray(function(err,taken){
+                //      //console.log(taken)
+                //      res.render('../views/manager_assignedClients.ejs',{OpenProjects:Open,TakenProjects:taken,Users:Clients});
+                // })
             });
             
         });
@@ -71,7 +72,12 @@ module.exports = function(app){
     app.get('/manager_addClients',function(req,res){
         res.render('../views/manager_addClients.ejs',{User:req.session.username});
     })
-    app.get('/manager_dashboard',function(req,res){
-        res.render('../views/manager_dashboard.ejs',{User:req.session.username});
+    app.get('/manager_viewProject',function(req,res){
+        var mongoUtil = require( '../../public/assets/scripts/mongdb' );
+        var db = mongoUtil.getDb();
+        db.collection('Projects').find({"Client":req.query.ClientName}).toArray(function(err,result){
+            res.render('../views/manager_viewProject.ejs',{Project:result,User:req.query.ClientName});
+        })
+        
     })
 }
--- routes/managerRoutes/postRoutes.js
+++ routes/managerRoutes/postRoutes.js
@@ -3,15 +3,32 @@ module.exports = function(app){
         //console.log(req.body);
         var mongoUtil = require( '../../public/assets/scripts/mongdb' );
         var db = mongoUtil.getDb();
-        db.collection('Projects').insertOne(req.body);
-        res.send("Successfully added");
+        console.log(req.body.ProjectName)
+        db.collection('Projects').findOne({"ProjectName":req.body.ProjectName},function(err,result){
+            if(result){
+                res.send("Project is already exist!")
+            }else{
+                db.collection('Projects').insertOne(req.body);
+                res.send("Successfully added");
+            }
+        })
+        //db.collection('Projects').insertOne(req.body);
+        // res.send("Successfully added");
     })
     app.post('/addClient',function(req,res){
         //console.log(req.body);
         var mongoUtil = require( '../../public/assets/scripts/mongdb' );
         var db = mongoUtil.getDb();
-        db.collection('User').insertOne(req.body);
-        res.send("Successfully added");
+        db.collection('User').findOne(req.body,function(err,result){
+            if(result){
+                res.send("Client is already exist!")
+            }else{
+                db.collection('User').insertOne(req.body);
+                res.send("Successfully added");
+            }
+        })
+       // db.collection('User').insertOne(req.body);
+       
     })
     app.post('/updateProgress',function(req,res){
         console.log(req.body)
@@ -24,37 +41,37 @@ module.exports = function(app){
            var Project = result.ProjectName;
            var num = Number(result.Progress)+1;
            db.collection('Projects').updateOne({'_id':ObjectId(req.body.id)},{$set:{"Progress":num}});
-           db.collection('User').findOne({"Status":"Assigned","FullName":who},function(err,result){
-             // console.log(result)
-               var pro ={
-                   FullName:result.FullName,
-                   Username:result.Username,
-                   Password:result.Password,
-                   Type:result.Type,
-                   DateAdded:result.DateAdded,
-                   Status:result.Status,
-                   Projects:result.Projects
-               };
+        //    db.collection('User').findOne({"Status":"Assigned","FullName":who},function(err,result){
+        //      // console.log(result)
+        //        var pro ={
+        //            FullName:result.FullName,
+        //            Username:result.Username,
+        //            Password:result.Password,
+        //            Type:result.Type,
+        //            DateAdded:result.DateAdded,
+        //            Status:result.Status,
+        //            Projects:result.Projects
+        //        };
 
-            var projectssss = result.Projects;
-            var projecccc = {
-                ProjectName:"",
-                Tasks:[],
-                DateAssigned:"",
-                Progress:0
-            }
-            var found = result.Projects.filter(function(item) { return item.ProjectName === Project ; });
-                found[0].Progress = num;
-               projecccc.ProjectName = found[0].ProjectName;
-               projecccc.Tasks = found[0].Tasks;
-               projecccc.DateAssigned = found[0].DateAssigned;
-               projecccc.Progress = found[0].Progress;
-            var noJohn = result.Projects.filter( el => el.ProjectName !== Project );
-            noJohn.push(projecccc);
-            pro.Projects = noJohn;
-             db.collection('User').deleteOne({"FullName":who});
-             db.collection('User').insertOne(pro);
-           })
+        //     var projectssss = result.Projects;
+        //     var projecccc = {
+        //         ProjectName:"",
+        //         Tasks:[],
+        //         DateAssigned:"",
+        //         Progress:0
+        //     }
+        //     var found = result.Projects.filter(function(item) { return item.ProjectName === Project ; });
+        //         found[0].Progress = num;
+        //        projecccc.ProjectName = found[0].ProjectName;
+        //        projecccc.Tasks = found[0].Tasks;
+        //        projecccc.DateAssigned = found[0].DateAssigned;
+        //        projecccc.Progress = found[0].Progress;
+        //     var noJohn = result.Projects.filter( el => el.ProjectName !== Project );
+        //     noJohn.push(projecccc);
+        //     pro.Projects = noJohn;
+        //      db.collection('User').deleteOne({"FullName":who});
+        //      db.collection('User').insertOne(pro);
+        //    })
         });
        // db.collection('Projects').updateOne({'_id':ObjectId(req.body.id)},{$set:{"Progress":"Assigned"}});
         //db.collection('User').insertOne(req.body);
@@ -80,55 +97,27 @@ module.exports = function(app){
         db.collection('User').findOne({'_id':ObjectId(req.body.idClient)},function(err,res){
             console.log(res)
             db.collection('Projects').updateOne({'_id':ObjectId(req.body.idProject)},{$set:{"Type":"Taken","Client":res.FullName,"DateAssigned":req.body.DateAssigned}})
-            var Projs = [];
-            if(res.Projects){
-                res.Projects.forEach(element => {
-                    Projs.push(element);
-               });
-            }
-                db.collection('Projects').findOne({'_id':ObjectId(req.body.idProject)},function(err,result){
-                    console.log(result)
-                    var ProjectN = result.ProjectName;
-                    Projs.push(result.Tasks);
-                    var proj = {
-                        ProjectName:ProjectN,
-                        Tasks:Projs,
-                        DateAssigned:req.body.DateAssigned,
-                        Progress:result.Progress
-                    }
-                    Projects.push(proj);
-                    db.collection('User').updateOne({'_id':ObjectId(req.body.idClient)},{$set:{"Status":"Assigned","Projects":Projects}});
-                })
+            // var Projs = [];
+            // if(res.Projects){
+            //     res.Projects.forEach(element => {
+            //         Projs.push(element);
+            //    });
+            // }
+                //db.collection('Projects').findOne({'_id':ObjectId(req.body.idProject)},function(err,result){
+                    // console.log(result)
+                    // var ProjectN = result.ProjectName;
+                    // Projs.push(result.Tasks);
+                    // var proj = {
+                    //     ProjectName:ProjectN,
+                    //     Tasks:Projs,
+                    //     DateAssigned:req.body.DateAssigned,
+                    //     Progress:result.Progress
+                    // }
+                    // Projects.push(proj);
+                    db.collection('User').updateOne({'_id':ObjectId(req.body.idClient)},{$set:{"Status":"Assigned"}});
+              //  })
             
         })
-
-        // db.collection('Projects').findOne({'_id':ObjectId(req.body.idProject)},function(err,result){
-        //     console.log(result.Tasks);
-        //     ProjName = result.ProjectName;
-        //     var proj = {
-        //         Client:"",
-        //         ProjectName:ProjName,
-        //         Tasks:[]
-        //     }
-        //     //console.log(ProjName)
-        //     db.collection('User').findOne({'_id':ObjectId(req.body.idClient)},function(err,res){
-        //       // console.log(res.FullName)
-        //        var Projs = [];
-        //        if(res.Projects){
-        //            //console.log("Defined")
-        //            res.Projects.forEach(element => {
-        //               Projs.push(element);
-        //            });
-        //            Projs.push(ProjName)
-
-        //        }else{
-        //            Projs.push(ProjName)
-        //        }
-        //       // db.collection('User').updateOne({'_id':ObjectId(req.body.idClient)},{$set:{"Status":"Assigned","DateAssigned":req.body.DateAssigned,"Projects":Projs}});
-        //        //db.collection('Projects').updateOne({'_id':ObjectId(req.body.idProject)},{$set:{"Type":"Taken","Client":res.FullName}})
-        //     })
-        // })
-        //db.collection('Projects').updateOne({'_id':ObjectId(req.body.idProject)},{$set:{"Status":"Taken","Projects":[]}});
         res.send("Successfully Assigned");
     })
 }
