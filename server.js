b7c435a571f2a3ab1fb6871aadeea8a7cfd09ce4
unknown <2ndyrGroupA@PN13.pn.local>
2019-05-22 00:04:44 +0800

    files modified

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 const assert = require('assert');
 const flash = require('connect-flash');
 const session = require('express-session');
