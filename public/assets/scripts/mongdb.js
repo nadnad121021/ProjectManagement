14196ae69242d67a9661209ba913c74defbcf3eb
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-26 14:56:02 +0800

    Files Added

--- public/assets/scripts/mongdb.js
+++ public/assets/scripts/mongdb.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require( 'mongodb' ).MongoClient;
-const url = "mongodb://172.16.8.29:27017/";
-//const url = "mongodb://172.16.32.40:27017/";
+// const url = "mongodb://172.16.8.29:27017/";
+const url = "mongodb://172.16.32.40:27017/";
 //const url = "mongodb://172.16.32.3:27017/";
 var _db;
 module.exports = {
